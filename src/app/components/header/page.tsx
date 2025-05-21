19880df9fea9dcb5542381e07cddc39c959bb91f
Caio José <caiojsdl16@gmail.com>
2025-05-20 23:08:37 -0300

    feat: header feito

---
+++ src/app/components/header/page.tsx
@@ -0,0 +1,29 @@
+import styles from './Header.module.css';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+        <div className={styles.logo}>
+            <Link href="/">M.</Link>
+        </div>
+
+        <nav className={styles.nav}>
+            <ul className={styles.navList}>
+                <li>
+                    <Link href="/" className={styles.navLink}>Home</Link>
+                </li>
+                <li className={styles.separatorRight}>
+                    <Link href="./artigo" className={styles.navLink}>Artigos</Link>
+                </li>
+                <li>
+                    <Link href="./artigo/new" className={styles.navLink}>Publicar</Link>
+                </li>
+                <li>
+                    <img src="/images/icon-avatar.png" alt="" className={styles.avatar}/>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
