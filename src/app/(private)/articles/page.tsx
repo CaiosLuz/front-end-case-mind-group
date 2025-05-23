1222cd2b5f2745a25f3c74c7a553d6f5e51153da
Caio José <caiojsdl16@gmail.com>
2025-05-22 23:51:32 -0300

    feat: lendo artigo do banco de dados

--- src/app/(private)/articles/page.tsx
+++ src/app/(private)/articles/page.tsx
@@ -1,21 +1,50 @@
 'use client';
 import styles from './Articles.module.css';
+import { useEffect, useState } from 'react';
+
+type Artigo = {
+  id: number,
+  titulo: string,
+  autor: 'John Doe',
+  conteudo: string,
+  imagem_url: string,
+};
 
 export default function Articles() {
 
-  const article = {
-    id: '01',
-    title:  'Dominando TypeScript: Por que a Tipagem Estática Está Transformando o Desenvolvimento JavaScript',
-    author: 'John Doe',
-    date: 'Março, 20, 2025',
-    content: 'TypeScript, uma superconjunto de JavaScript, tem se tornado uma escolha popular entre desenvolvedores para garantir código mais seguro e fácil de manter. Neste artigo, vamos explorar os benefícios da tipagem estática no...',
-    image: '/images/artigoImagem.png',
-  };
+  const [artigos, setArtigos] = useState<Artigo[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/artigos')
+    .then(res => res.json())
+    .then(data => setArtigos(data))
+    .catch(err => console.error(err));
+  }, []);
 
   return (
     <main className={styles.main}>
 
-      <div className={styles.card}>
+      {artigos.map((artigo) => (
+        <div className={styles.card} key={artigo.id}>
+          <div >
+            <img src={artigo.imagem_url} alt="Imagem principal" className={styles.imagemPrincipal}/>
+          </div>
+          <div className={styles.tituloPrincipal}>
+            <p>{artigo.titulo}</p>
+          </div>
+          <div className={styles.content}>
+            <p>{artigo.conteudo}</p>
+          </div>
+          <div className={styles.blocoInfoPrincipal}>
+            <div className={styles.infoDestaque}>
+              <img src="/images/icon-avatar.png" alt="" className={styles.avatar}/>
+              <p>Por {artigo.autor} - Maio, 22, 2025</p>
+            </div>
+          </div>
+        </div>
+      ))}
+
+      {/* <div className={styles.card}>
         <div >
           <img src={article.image} alt="Imagem principal" className={styles.imagemPrincipal}/>
         </div>
@@ -103,25 +132,7 @@ export default function Articles() {
             <p>Por {article.author} - {article.date}</p>
           </div>
         </div>
-      </div>
-
-      <div className={styles.card}>
-        <div >
-          <img src={article.image} alt="Imagem principal" className={styles.imagemPrincipal}/>
-        </div>
-        <div className={styles.tituloPrincipal}>
-          <p>{article.title}</p>
-        </div>
-        <div className={styles.content}>
-          <p>{article.content}</p>
-        </div>
-        <div className={styles.blocoInfoPrincipal}>
-          <div className={styles.infoDestaque}>
-            <img src="/images/icon-avatar.png" alt="" className={styles.avatar}/>
-            <p>Por {article.author} - {article.date}</p>
-          </div>
-        </div>
-      </div>
+      </div> */}
 
     </main> 
   );
